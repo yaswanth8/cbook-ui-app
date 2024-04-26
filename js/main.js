7db2bc83ffb4d97511cd682be002156dc610f111
yaswanth8 <ykondaveeti@gmail.com>
2024-04-25 20:11:26 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const idContactList = document.getElementById('idContactList');
+idContactList.innerHTML = 'Contact list will be displayed here...';
+
+//const basePath = "http://localhost:8080";
+const basePath = "https://cbook-server-app-usci.onrender.com";
+
+const getContacts = async () => {
+    const response = await fetch(`${basePath}/api/contact/all`);
+    const contacts = await response.json();
+    return contacts;
+}
+
+const showContacts = async () => {
+    const contacts = await getContacts();
+    // Create a table
+
+    let table = '<table border="1" class="table table-striped">';
+    table += '<tr>';
+    table += '<th>Name</th>';
+    table += '<th>Email</th>';
+    table += '<th>Mobile</th>';
+    table += '<th>Address</th>';
+    table += '<th>Edit</th>';
+    table += '<th>Delete</th>';
+    table += '</tr>';
+
+    contacts.forEach(contact => {
+        table += '<tr>';
+        table += `<td>${contact.name}</td>`;
+        table += `<td>${contact.email}</td>`;
+        table += `<td>${contact.mobile}</td>`;
+        table += `<td>${addressToStr(contact.address)}</td>`;
+        table += `<td><a href="edit-contact.html?id=${contact.id}">Edit</a></td>`;  
+        table += `<td><a href="delete-contact.html?id=${contact.id}">Delete</a></td>`;  
+        table += '</tr>';
+    });
+
+    table += '</table>';
+    idContactList.innerHTML = table;
+}
+
+const addressToStr = (address) => {
+    let addressStr = '';
+    addressStr += address.city + ', ';
+    addressStr += address.state + ', ';
+    addressStr += address.zipCode;
+    addressStr += ', ' + address.country;
+    return addressStr;
+}
+showContacts();
